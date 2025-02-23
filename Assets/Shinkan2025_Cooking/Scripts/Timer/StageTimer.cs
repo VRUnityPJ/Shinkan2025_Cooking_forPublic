f964ce370404ffe615997b8042d4f33af6817fbf
庄子 <>
2025-02-23 17:55:54 +0900

    [feature] StageTimerを新歓2024からぶっこ抜いた ビルドしなくてもテストできるようにInspecterから動作確認可能

---
+++ Assets/Shinkan2025_Cooking/Scripts/Timer/StageTimer.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Shinkan2025_Cooking.Scripts.Timer
+{
+    public class StageTimer : MonoBehaviour
+    {
+        public IObservable<float> OnTimerStart => _onTimerStart;
+
+        [SerializeField] private float _time = 5f;
+        [SerializeField] private UnityEvent _timeUpEvent = new();
+        private readonly Subject<float> _onTimerStart = new();
+        private CancellationToken _token = new();
+
+        private void Start()
+        {
+            _token = this.GetCancellationTokenOnDestroy();
+
+        }
+
+        [ContextMenu("StartTimer")]
+        public void StartTimer()
+        {
+            StartTimerAsync(_token);
+        }
+
+        public async void StartTimerAsync(CancellationToken token)
+        {
+            _onTimerStart.OnNext(_time);
+            await UniTask.Delay(TimeSpan.FromSeconds(_time), cancellationToken: _token);
+            _timeUpEvent.Invoke();
+            Debug.Log("ゲーム終了");
+        }
+    }
+}
