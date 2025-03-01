c159ed9b769a21cae68d6a834ddab9bb29ca1c92
庄子 <>
2025-03-01 14:14:12 +0900

    [feature] 完成した料理名がアニメーションで表示できるようになった。

--- Assets/Shinkan2025_Cooking/Scripts/Vcontainer/GameLifeTimeScope.cs
+++ Assets/Shinkan2025_Cooking/Scripts/Vcontainer/GameLifeTimeScope.cs
@@ -4,19 +4,25 @@ using Shinkan2025_Cooking.Scripts.Timer;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
+using Shinkan2025_Cooking.Scripts.Points;
 
 public class GameLifeTimeScope : LifetimeScope
 {
     [SerializeField] private GameProgress _gameProgress;
     [SerializeField] private HogeSwordSpawner _swordSpawner;
+    [SerializeField] private ComboPresenter _comboPresenter;
 
     protected override void Configure(IContainerBuilder builder)
     {
         RegisterCompenentInHierarchy<DemoGameEnd>(builder);
 
+        builder.RegisterComponentInHierarchy<RecipeChecker>()
+          .As<IRecipeObservable>();
+
         //Inject先のクラスのインスタンス登録
         builder.RegisterComponent(_swordSpawner);
         builder.RegisterComponent(_gameProgress);
+        builder.RegisterComponent(_comboPresenter);
     }
 
     /// <summary>
