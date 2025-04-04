1cf57f95961105f04e920aecf3dd8ae7ae13fcb6
ARIHI-8 <0ali8hi26@gmail.com>
2025-04-04 15:43:21 +0900

    しょーじシーン調整、ゲーム終了の待ち時間設定

--- Assets/Shinkan2025_Cooking/Scripts/GameCore/GameProgressStateController.cs
+++ Assets/Shinkan2025_Cooking/Scripts/GameCore/GameProgressStateController.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 using UniRx;
 using Cysharp.Threading.Tasks;
+using System.Threading;
 using Shinkan2025_Cooking.Scripts.StateMachine;
 
 namespace Shinkan2025_Cooking.Scripts.GameCore
@@ -16,17 +17,26 @@ namespace Shinkan2025_Cooking.Scripts.GameCore
         private Subject<Unit> _onStartGame = new Subject<Unit>();
         
         private Subject<Unit> _onEndGame = new Subject<Unit>();
+        private CancellationToken _token = new();
+        [SerializeField] private float _time = 5f;
 
         public void StartGame()
         {
             _onStartGame.OnNext(Unit.Default);
             _onStartGame.OnCompleted();
+            _token = this.GetCancellationTokenOnDestroy();
         }
 
         public void EndGame()
         {
+            EndGameAsync(_token);
+        }
+        public async void EndGameAsync(CancellationToken token)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(_time), cancellationToken: token);
             _onEndGame.OnNext(Unit.Default);
             _onEndGame.OnCompleted();
+            Debug.Log("ゲーム終了");
         }
     }
 }
