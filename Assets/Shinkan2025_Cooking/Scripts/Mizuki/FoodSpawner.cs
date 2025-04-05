b60c92e0cf833d8d63af93c2c44e52ddb369825c
ARIHI-8 <0ali8hi26@gmail.com>
2025-04-05 22:08:54 +0900

    アニメーション再生、日本語フォント調整

--- Assets/Shinkan2025_Cooking/Scripts/Mizuki/FoodSpawner.cs
+++ Assets/Shinkan2025_Cooking/Scripts/Mizuki/FoodSpawner.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 using VContainer;
 using UniRx;
 using Unity.VisualScripting;
-
+using UnityEngine.Playables;
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 public class FoodSpawner : MonoBehaviour
 {
     [SerializeField] private IGameProgressIndicatable _gameProgressIndicatable;
@@ -12,11 +15,14 @@ public class FoodSpawner : MonoBehaviour
     [SerializeField] private float foodSpeed = 5.0f;
     [SerializeField] private Transform[] spawnPoints;
     [SerializeField] private float foodLifeTime = 10.0f;
+    [SerializeField] private List<PlayableDirector> _directors;
+    [SerializeField] private float _time = 0.9f;
     
     private GameObject food;
     private float time = 0.0f;
     private float nextFoodSpawnTime = 1.0f;
     public bool isFoodSpawnable  = false;
+    private CancellationToken _token = new();
 
     void Awake()
     {
@@ -46,8 +52,9 @@ public class FoodSpawner : MonoBehaviour
         time += Time.deltaTime;
         if (time >= nextFoodSpawnTime)
         {
+            _token = this.GetCancellationTokenOnDestroy();
             NextFood();
-            int randomPointFirst = Random.Range(0, spawnPoints.Length);
+            int randomPointFirst = UnityEngine.Random.Range(0, spawnPoints.Length);
             int randomPointSecond;
 
             // 無限ループを避けるため、範囲が2以上であることを確認
@@ -55,7 +62,7 @@ public class FoodSpawner : MonoBehaviour
             {
                 do
                 {
-                    randomPointSecond = Random.Range(0, spawnPoints.Length);
+                    randomPointSecond = UnityEngine.Random.Range(0, spawnPoints.Length);
                 } while (randomPointFirst == randomPointSecond);
             }
             else
@@ -63,12 +70,28 @@ public class FoodSpawner : MonoBehaviour
                 // 範囲が1以下の場合、安全にデフォルト値を設定
                 randomPointSecond = randomPointFirst;
             }
+            //0秒
+            //アニメーション開始
+            _directors[randomPointFirst].Play();
+            _directors[randomPointSecond].Play();
+            //炒める時間まつ
 
-            InstantiateFood(randomPointFirst);
-            InstantiateFood(randomPointSecond);
+           
+            InstantiateFoodAsync(_token,randomPointFirst);
+            InstantiateFoodAsync(_token,randomPointSecond);
             time = 0.0f;
         }
     }
+    public async void InstantiateFoodAsync(CancellationToken token,int spawnPointNum)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(_time), cancellationToken: token);
+            Transform randomSpawnPoint = spawnPoints[spawnPointNum];
+
+            GameObject spawnedFood = Instantiate(food, randomSpawnPoint);
+            spawnedFood.TryGetComponent<IFoodPhysicsHandler>(out var handler);
+            handler.OnInstantiate(foodSpeed, foodLifeTime);
+            Debug.Log("timeゲーム終了");
+        }
     private void InstantiateFood(int spawnPointNum)
     {
         
@@ -82,9 +105,9 @@ public class FoodSpawner : MonoBehaviour
 
     private void NextFood()
     {
-        nextFoodSpawnTime = Random.Range(2.5f, 5.0f);
+        nextFoodSpawnTime = UnityEngine.Random.Range(2.5f, 5.0f);
         //foodSpeed = Random.Range(5.0f, 10.0f);
-        int index = Random.Range(0, foodObjList.foodList.Count);
+        int index = UnityEngine.Random.Range(0, foodObjList.foodList.Count);
         food = foodObjList.foodList[index];
     }
 }
